4ebe8ff891abc5beaa2c2972be9620cc94192a74
Pranas Varaneckas <varaneckas@gmail.com>
2021-10-12 23:04:20 +0300

    created person model

--- myApp.js
+++ myApp.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
+const { Schema } = require('mongoose');
 var mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
-let Person;
+const personSchema = new Schema({
+    name: {type: String, required: true},
+    age: Number,
+    favoriteFoods: [String]
+});
+const Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
