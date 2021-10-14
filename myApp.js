269565a6044b1752ce7664fffcefa47392bd77e1
Pranas Varaneckas <varaneckas@gmail.com>
2021-10-14 21:52:18 +0300

    findoneandupdate

--- myApp.js
+++ myApp.js
@@ -79,7 +79,15 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    {name: personName},
+    {age: ageToSet},
+    {new: true},
+    (err, updatedDocument) => {
+      if (err) console.error(err);
+      done(null /*, data*/);
+    }
+  );
 };
 
 const removeById = (personId, done) => {
